96f6bf4aa0cef39adac0c8356ace88b5ab41f850
Adriano Massi <adrianomassi@gmail.com>
2018-10-19 16:12:56 +0200

    Lezione 37

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,24 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(MyApp());
+import './product_manager.dart';
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
+void main() => runApp(MyApp());
 
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple,
+      ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    _products.add('Advanced Food Tester');
-                    print(_products);
-                  },
-                  child: Text('Add Product'),
-                ),
-              ),
-              Column(
-                children: _products
-                    .map((element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpg'),
-                              Text(element)
-                            ],
-                          ),
-                        ))
-                    .toList(),
-              )
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(),
+      ),
     );
   }
 }
