ce2f1e72c0d7c9b5288ae6e0f1f36082dd418846
Adriano Massi <adrianomassi@gmail.com>
2018-11-02 02:44:20 +0100

    Lesson 79 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 //import 'package:flutter/rendering.dart';
 
 import './pages/auth.dart';
+import './pages/products_admin.dart';
+import './pages/products.dart';
 
 void main() {
-
- /*  debugPaintSizeEnabled = true;
+  /*  debugPaintSizeEnabled = true;
   debugPaintBaselinesEnabled = true; 
   debugPaintPointersEnabled = true; */
   runApp(MyApp());
@@ -15,13 +16,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //debugShowMaterialGrid: true,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple,
-      ),
-      home: AuthPage(),
-    );
+        //debugShowMaterialGrid: true,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple,
+        ),
+        //home: AuthPage(),
+        routes: {
+          '/': (BuildContext context) => ProductsPage(),
+          '/admin': (BuildContext context) => ProductsAdminPage(),
+        });
   }
 }
