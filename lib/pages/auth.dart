140e710ca813579a8282ae1efd937c04268a05a7
Adriano Massi <adrianomassi@gmail.com>
2018-11-08 01:56:39 +0100

    Section 8 Completed

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -14,74 +14,98 @@ class _AuthPageState extends State<AuthPage> {
   String _passwordValue = '';
   bool _acceptTerms = false;
 
+  DecorationImage _buildBackgroundImage() {
+    return DecorationImage(
+      fit: BoxFit.cover,
+      colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+      image: AssetImage('assets/background.jpg'),
+    );
+  }
+
+  Widget _buildEmailTextField() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Email',
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (String value) {
+        setState(() {
+          _emailValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        filled: true,
+        fillColor: Colors.white,
+      ),
+      onChanged: (String value) {
+        setState(() {
+          _passwordValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildAcceptSwitch() {
+    return SwitchListTile(
+      value: _acceptTerms,
+      onChanged: (bool value) {
+        setState(() {
+          _acceptTerms = value;
+        });
+      },
+      title: Text('Accept Terms'),
+    );
+  }
+
+  void _submitForm() {
+    print("Email $_emailValue");
+    print("Password $_passwordValue");
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 768.0 ? 500.0 : deviceWidth * 0.95;
     return Scaffold(
         appBar: AppBar(
           title: Text('Login'),
         ),
         body: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5), BlendMode.dstATop),
-              image: AssetImage('assets/background.jpg'),
-            ),
+            image: _buildBackgroundImage(),
           ),
           padding: EdgeInsets.all(10.0),
           child: Center(
             child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      filled: true,
-                      fillColor: Colors.white,
+              child: Container(
+                width: targetWidth,
+                child: Column(
+                  children: <Widget>[
+                    _buildEmailTextField(),
+                    SizedBox(
+                      height: 10.0,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: (String value) {
-                      setState(() {
-                        _emailValue = value;
-                      });
-                    },
-                  ),
-                  SizedBox(height: 10.0,),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                    onChanged: (String value) {
-                      setState(() {
-                        _passwordValue = value;
-                      });
-                    },
-                  ),
-                  SwitchListTile(
-                    value: _acceptTerms,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _acceptTerms = value;
-                      });
-                    },
-                    title: Text('Accept Terms'),
-                  ),
-                  SizedBox(height: 10.0),
-                  RaisedButton(
-                    child: Text('Login'),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      print("Email $_emailValue");
-                      print("Password $_passwordValue");
-                      Navigator.pushReplacementNamed(context, '/products');
-                    },
-                  )
-                ],
+                    _buildPasswordTextField(),
+                    _buildAcceptSwitch(),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      child: Text('Login'),
+                      textColor: Colors.white,
+                      onPressed: _submitForm,
+                    )
+                  ],
+                ),
               ),
             ),
           ),
--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -11,32 +11,29 @@ class ProductPage extends StatelessWidget {
   final String imageUrl;
 
   ProductPage(this.title, this.price, this.description, this.imageUrl);
-/* 
-  _showWarningDialog(BuildContext context) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Are you sure?'),
-            content: Text('This action cannot be undone!'),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('DISCARD'),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              FlatButton(
-                child: Text('DELETE'),
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.pop(context, true);
-                },
-              )
-            ],
-          );
-        });
-  } */
+
+  Widget _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Union Square San Francisco',
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          '\$' + price.toString(),
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,26 +58,7 @@ class ProductPage extends StatelessWidget {
                   child: TitleDefault(title),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Union Square San Francisco',
-                    style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Text(
-                      '|',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                  Text(
-                    '\$' + price.toString(),
-                    style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
-                  ),
-                ],
-              ),
+              _buildAddressPriceRow(),
               Container(
                 padding: EdgeInsets.all(10.0),
                 child: Text(
